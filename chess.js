cd81eac7e9fb5c56ebff33deffac780310d3158f
Nikhil <vallalanikhil@gmail.com>
2020-07-18 13:17:35 +0530

    queen movements

---
+++ chess.js
@@ -0,0 +1,119 @@
+var queen = {
+  position: {
+    x: 0,
+    y: 0,
+  },
+};
+
+function changedirection(num, direction) {
+  var temp;
+  var temp1;
+  switch (direction) {
+    case "E":
+      temp = queen.position.y + num; //N
+      if (temp > 7 || temp < 0) {
+        console.log("outside boundary E");
+        document.write("outside boundary");
+      } else {
+        queen.position.y = temp;
+      }
+      break;
+    case "W":
+      temp = queen.position.y - num;
+      if (temp > 7 || temp < 0) {
+        console.log("outside boundary W");
+        document.write("outside boundary");
+      } else {
+        queen.position.y = temp;
+      }
+      break;
+    case "S":
+      temp = queen.position.x + num;
+      if (temp > 7 || temp < 0) {
+        console.log("outside boundary S");
+        document.write("outside boundary");
+      } else {
+        queen.position.x = temp;
+      }
+      break;
+    case "N":
+      temp = queen.position.x - num;
+      if (temp > 7 || temp < 0) {
+        console.log("outside boundary N");
+        document.write("outside boundary");
+      } else {
+        queen.position.x = temp;
+      }
+      break;
+    case "NE": //N2E8
+      temp = queen.position.x - num;
+      temp1 = queen.position.y + num;
+      if (temp > 7 || temp < 0 || temp1 > 7 || temp1 < 0) {
+        console.log("outside boundary ");
+        document.write("outside boundary");
+      } else {
+        queen.position.x = temp;
+        queen.position.y = temp1;
+      }
+      break;
+    case "SE":
+      temp = queen.position.x + num;
+      temp1 = queen.position.y + num;
+
+      if (temp > 7 || temp < 0 || temp1 > 7 || temp1 < 0) {
+        console.log("outside boundary ");
+        document.write("outside boundary");
+      } else {
+        queen.position.x = temp;
+        queen.position.y = temp1;
+      }
+      break;
+    case "SW":
+      temp = queen.position.x + num;
+      temp1 = queen.position.y - num;
+      if (temp > 7 || temp < 0 || temp1 > 7 || temp1 < 0) {
+        console.log("outside boundary ");
+        document.write("outside boundary");
+      } else {
+        queen.position.x = temp;
+        queen.position.y = temp1;
+      }
+      break;
+    case "NW": //N2W5
+      temp = queen.position.x - num;
+      temp1 = queen.position.y - num;
+      if (temp > 7 || temp < 0 || temp1 > 7 || temp1 < 0) {
+        console.log("outside boundary ");
+        document.write("outside boundary");
+      } else {
+        queen.position.x = temp;
+        queen.position.y = temp1;
+      }
+      break;
+  }
+}
+function play(input) {
+  var value = input.split(" "); //split() method is used to split a string into an array of substrings, and returns the new array//
+  value.forEach((Element) => {
+    //forEach() method calls a function once for each element in an array, in order
+
+    let num = Number(Element.slice(Element.length - 1)); //slice() method selects the elements starting at the given start argument, and ends at, but does not include, the given end argument.
+    let direction = Element.slice(0, Element.length - 1);
+    //console.log("Number =>" + num)
+    //console.log("direction =>" + direction)
+
+    changedirection(num, direction);
+  });
+  console.log("Your Position => " + broard[queen.position.x][queen.position.y]);
+}
+
+var broard = [
+  ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
+  ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
+  ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
+  ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
+  ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
+  ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
+  ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+  ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
+];
